f72056f3daa5b163e740bf63cad15f63e1cbac4a
Jomalkeun <Jomalkeun@gmail.com>
2014-09-11 18:06:11 +0900

    no message

--- js/malkeun.js
+++ js/malkeun.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
-	
+	brain();
 });
+
+function brain(){
+	var current = 0;
+	var brain = $(".brain .inner");
+
+	setInterval(function(){
+		/*
+		var prev = brain.eq(current);
+		prev.css("top", 0)
+			.stop()
+			.animate({top:'-40px'}, {duration:0});
+		*/
+
+		current ++;
+
+		if (current == 13) current = 0;
+		console.log(current);
+		var next = brain.eq(current);
+		var height = 40 * current;
+		var move = "-" + height + "px"
+		next.css("top", move)
+			.stop()
+			.animate({top:move}, {duration:0});
+		console.log(move);
+	}, 100);
+}
+
+
+
+
+
+
+
+
+
+
+
